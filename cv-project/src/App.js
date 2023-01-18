12e5a6c80ee98b01a69cff943135df0523a64bde
Jerry Wang <jerry.wang2352@gmail.com>
2023-01-17 22:28:56 -0500

    Create Header

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -1,7 +1,11 @@
+import React from 'react'; 
+import Header from './components/header'
+import './styles/App.css' 
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
     </div>
   );
 }
---
+++ cv-project/src/components/header.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import '../styles/Header.css'
+
+class Header extends Component {
+    render() {
+        return( 
+            <div className='header'>CV Creator</div>
+        );
+    }
+}
+
+export default Header; 
